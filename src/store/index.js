b89327a30fd91c0357544678af35e5fed6ed9cb3
I'm Jaystar <58359895+Jaystar-Bee@users.noreply.github.com>
2022-10-23 16:23:15 +0100

    debugging tailwind issue: updated tailwind

--- src/store/index.js
+++ src/store/index.js
@@ -9,71 +9,7 @@ export default createStore({
       cart: {
         shippingCost: 1000,
         vatCost: 50,
-        items: [
-          {
-            id: 1,
-            new: true,
-            section: "headphones",
-            title: "XX99 Mark II Headphones",
-            quantity: 1,
-            img: "headphone-product-1",
-            description:
-              "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
-            feature1: "Featuring a genuine leather head strap and premium earcups, these headphones deliver superior comfort for those who like to enjoy endless listening.It includes intuitive controls designed for any situation.Whether you’re taking a business call or just in your own personal space, the auto on/ off and pause features ensure that you’ll never miss a beat.",
-            feature2: "The advanced Active Noise Cancellation with built -in equalizer allow you to experience your audio world on your terms.It lets you enjoy your audio in peace, but quickly interact with your surroundings when you need to.Combined with Bluetooth 5. 0 compliant connectivity and 17 hour battery life, the XX99 Mark II headphones gives you superior sound, cutting- edge technology, and a modern design aesthetic.",
-            priceInDollars: 2999,
-            total: 2999,
-            gallery: ['headphone-product-1-picture-1', "headphone-product-1-picture-2", "headphone-product-1-picture-3"],
-            mayLike: [
-              {
-                id: 1,
-                realLink: '/headphones/1',
-                img: 'headphone-product-1',
-                title: 'XX99 Mark II'
-              },
-              {
-                id: 2,
-                realLink: '/headphones/3',
-                img: 'headphone-product-3',
-                title: 'XX59'
-              },
-              {
-                id: 3,
-                realLink: '/speakers/1',
-                img: 'speaker-product-1',
-                title: 'ZX9 speaker'
-              },
-            ],
-            inTheBox: [
-              {
-                id: 1,
-                quantity: 1,
-                name: 'Headphone Unit',
-              },
-              {
-                id: 2,
-                quantity: 2,
-                name: 'Replacement Earcups',
-              },
-              {
-                id: 3,
-                quantity: 1,
-                name: 'User Manual',
-              },
-              {
-                id: 4,
-                quantity: 1,
-                name: '3.5mm 5m Audio Cable',
-              },
-              {
-                id: 5,
-                quantity: 1,
-                name: 'Travel Bag',
-              },
-
-            ]
-          }
-        ],
+        items: [],
 
       }
     }
