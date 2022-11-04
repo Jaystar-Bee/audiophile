56df087ffcc89ac43e6346d46c36b5630de56b49
I'm Jaystar <58359895+Jaystar-Bee@users.noreply.github.com>
2022-11-04 12:41:08 +0100

    added little dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   purge: [
-//     "./index.html",
-//     "./src/**/*.{vue,js,ts,jsx,tsx}",
-//     "./src/components/**/ *.vue",
-//     "./components/**/*.vue",
-//     "./src/views/**/*.vue",
-//     "./views/**/*.vue",
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         "deep-brown": "#D87D4A",
-//         "light-brown": "#fbaf85",
-//         "deep-black": "#101010",
-//         grey: "#F1F1F1",
-//         "light-grey": "#FAFAFA",
-//       },
-//     },
-//     fontFamily: {
-//       Manrope: ["Manrope", "sans-serif"],
-//     },
-//     backgroundSize: {
-//       "100": "100% 100%",
-//       "110": "110%",
-//       "125": "125%",
-//       "150": "150%",
-//       "175": "175%"
-//     },
-//   },
-//   plugins: [],
-// };
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
@@ -68,3 +35,39 @@ module.exports = {
     },
   },
 }
+
+// Old config
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   purge: [
+//     "./index.html",
+//     "./src/**/*.{vue,js,ts,jsx,tsx}",
+//     "./src/components/**/ *.vue",
+//     "./components/**/*.vue",
+//     "./src/views/**/*.vue",
+//     "./views/**/*.vue",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         "deep-brown": "#D87D4A",
+//         "light-brown": "#fbaf85",
+//         "deep-black": "#101010",
+//         grey: "#F1F1F1",
+//         "light-grey": "#FAFAFA",
+//       },
+//     },
+//     fontFamily: {
+//       Manrope: ["Manrope", "sans-serif"],
+//     },
+//     backgroundSize: {
+//       "100": "100% 100%",
+//       "110": "110%",
+//       "125": "125%",
+//       "150": "150%",
+//       "175": "175%"
+//     },
+//   },
+//   plugins: [],
+// };
