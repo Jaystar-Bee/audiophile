eb71b5375aeb2aee80a51435d0cc97ba7beb4573
I'm Jaystar <58359895+Jaystar-Bee@users.noreply.github.com>
2022-11-04 12:13:20 +0100

    updated the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,44 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   purge: [
+//     "./index.html",
+//     "./src/**/*.{vue,js,ts,jsx,tsx}",
+//     "./src/components/**/ *.vue",
+//     "./components/**/*.vue",
+//     "./src/views/**/*.vue",
+//     "./views/**/*.vue",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         "deep-brown": "#D87D4A",
+//         "light-brown": "#fbaf85",
+//         "deep-black": "#101010",
+//         grey: "#F1F1F1",
+//         "light-grey": "#FAFAFA",
+//       },
+//     },
+//     fontFamily: {
+//       Manrope: ["Manrope", "sans-serif"],
+//     },
+//     backgroundSize: {
+//       "100": "100% 100%",
+//       "110": "110%",
+//       "125": "125%",
+//       "150": "150%",
+//       "175": "175%"
+//     },
+//   },
+//   plugins: [],
+// };
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  purge: [
+  content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./src/components/**/ *.vue",
-    "./components/**/*.vue",
-    "./src/views/**/*.vue",
-    "./views/**/*.vue",
+    "./src/**/*.{vue,js}",
   ],
   theme: {
-    extend: {
-      colors: {
-        "deep-brown": "#D87D4A",
-        "light-brown": "#fbaf85",
-        "deep-black": "#101010",
-        grey: "#F1F1F1",
-        "light-grey": "#FAFAFA",
-      },
-    },
     fontFamily: {
       Manrope: ["Manrope", "sans-serif"],
     },
@@ -28,6 +49,22 @@ module.exports = {
       "150": "150%",
       "175": "175%"
     },
+    extend: {
+      screens: {
+        "xs": '350px',
+        ...defaultTheme.screens
+      },
+      height: {
+        '90v': '80vh',
+        '70v': '70vh'
+      },
+      colors: {
+        "deep-brown": "#D87D4A",
+        "light-brown": "#fbaf85",
+        "deep-black": "#101010",
+        'grey': "#F1F1F1",
+        "light-grey": "#FAFAFA",
+      },
+    },
   },
-  plugins: [],
-};
+}
